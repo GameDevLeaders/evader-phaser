37fdd01dbc7c95e916692284bbd9c3d1383c097d
tblanchet <tblanchet@switchfly.com>
2015-08-08 02:06:36 +0000

    animation system works, just missing correct timing and sprites

--- src/scripts/states/intro.js
+++ src/scripts/states/intro.js
@@ -26,12 +26,11 @@ function introState () {
 		this.game.load.image('mouse-jetpack', 'assets/intro-animation/mouse-jetpack.png');
 		this.game.load.image('princesssurprise', 'assets/intro-animation/princesssurprise.png');
 		this.game.load.image('bg-sky', 'assets/intro-animation/sky.png');
-		this.game.load.image('bg-towerup', 'assets/intro-animation/tower-up.png');
-		this.game.load.image('bg-towerdown', 'assets/intro-animation/tower-down.png');
+		this.game.load.image('towerup', 'assets/intro-animation/tower-up.png');
+		this.game.load.image('towerdown', 'assets/intro-animation/tower-down.png');
 		this.game.load.spritesheet('hada', 'assets/intro-animation/hada.png', c.HADA_WIDTH,  c.HADA_HEIGHT, c.HADA_SPRITES);
 		this.game.load.spritesheet('mouse', 'assets/intro-animation/mouse.png', c.MOUSE_WIDTH,  c.MOUSE_HEIGHT, c.MOUSE_SPRITES);
 		this.game.load.spritesheet('prince', 'assets/intro-animation/prince.png', c.PRINCE_WIDTH,  c.PRINCE_HEIGHT, c.PRINCE_SPRITES);
-		console.log('hola' + c.PRINCE_WIDTH);
 		this.game.load.spritesheet('princessgrabbed', 'assets/intro-animation/princessGrabbed.png', c.PRINCESSGRABBED_WIDTH,  c.PRINCESSGRABBED_HEIGHT, c.PRINCESSGRABBED_SPRITES);
 		this.game.load.spritesheet('princesslooking', 'assets/intro-animation/princesslooking.png', c.PRINCESSLOOKING_WIDTH,  c.PRINCESSLOOKING_HEIGHT, c.PRINCESSLOOKING_SPRITES);
 
@@ -40,32 +39,26 @@ function introState () {
 	function create () {
 
 		this.game.add.sprite(this.game.world.centerX - 200, this.game.world.centerY/2, 'bg-castle');
-		var prince = this.game.add.sprite(180, 220, 'prince');
+		var prince = this.game.add.sprite(this.game.world.centerX - 200, this.game.world.centerY/2, 'prince');
 		prince.animations.add('act');
 		prince.animations.play('act', 2, true);
-		//this.game.time.events.add(Phaser.Timer.SECOND * 4.1, function () {addFrame(this, 'bg-sky', 6.3)}, this);
-		//this.game.time.events.add(Phaser.Timer.SECOND * 6.3, function () {addFrame(this, 'bg-grass', 11.7)}, this);
-		//this.game.time.events.add(Phaser.Timer.SECOND * 11.7, function () {addFrame(this, 'bg-closetower', 15)}, this);
-		this.game.time.events.add(Phaser.Timer.SECOND * 15, function () {addFrame(this, 'bg-woods', 18, 200, 200, 'mouse')}, this);
-		//this.game.time.events.add(Phaser.Timer.SECOND * 18, function () {addFrame(this, 'bg-fartower', 20.6)}, this);
-		// this line has some error
-		//this.game.time.events.add(Phaser.Timer.SECOND * 20.6, function () {addFrame(this, 'bg-sky', 25)}, this);
+		this.game.time.events.add(Phaser.Timer.SECOND * 4.1, function () {addFrame(this, 'bg-sky', 6.3, this.game.world.centerX - 200, this.game.world.centerY/2, 'towerup')}, this);
+		this.game.time.events.add(Phaser.Timer.SECOND * 6.3, function () {addFrame(this, 'bg-grass', 11.7, this.game.world.centerX - 200, this.game.world.centerY/2, 'towerdown')}, this);
+		this.game.time.events.add(Phaser.Timer.SECOND * 11.7, function () {addFrame(this, 'bg-closetower', 15, this.game.world.centerX - 200, this.game.world.centerY/2, 'princesslooking')}, this);
+		this.game.time.events.add(Phaser.Timer.SECOND * 15, function () {addFrame(this, 'bg-woods', 18, this.game.world.centerX - 200, this.game.world.centerY/2, 'hada')}, this);
+		this.game.time.events.add(Phaser.Timer.SECOND * 18, function () {addFrame(this, 'bg-woods', 20.6, this.game.world.centerX - 200, this.game.world.centerY/2, 'mouse')}, this);
+		this.game.time.events.add(Phaser.Timer.SECOND * 18, function () {addFrame(this, 'bg-fartower', 20.6, this.game.world.centerX - 200, this.game.world.centerY/2, 'princessgrabbed')}, this);
+		this.game.time.events.add(Phaser.Timer.SECOND * 20.6, function () {addFrame(this, 'bg-sky', 25, this.game.world.centerX - 200, this.game.world.centerY/2, 'flyingprincess')}, this);
 		this.game.time.events.add(Phaser.Timer.SECOND * 25, startGame, this);
 
 
 	}
 
-	function addFrame (that, bgimage, timeOnStage, spriteX, spriteY, sprites) { //add var type, if background or if animation or both, vars background and animated sprite
-		//debugger
+	function addFrame (that, bgimage, timeOnStage, spriteX, spriteY, sprites) {
 		background = that.game.add.sprite(that.game.world.centerX - 200, that.game.world.centerY/2, bgimage);
 		characters = that.game.add.sprite(spriteX, spriteY, sprites);
 		characters.animations.add('animate');
 		characters.animations.play('animate', 4, true);
-		// that.game.time.events.add(Phaser.Timer.SECOND * timeOnStage, function () {removeFrame(frame)}, that);
-	}
-
-	function removeFrame (image) {
-		image.destroy();
 	}
 
 	function update () {
