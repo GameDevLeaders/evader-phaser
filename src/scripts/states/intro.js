d8c0625f9dc7eac75e2a1f5e1a93d81cdbb7022a
tblanchet <tblanchet@switchfly.com>
2015-08-11 01:35:30 +0000

    full movie ready

--- src/scripts/states/intro.js
+++ src/scripts/states/intro.js
@@ -22,12 +22,12 @@ function introState () {
 		this.game.load.image('bg-fartower', 'assets/intro-animation/background-towerfar.png');
 		this.game.load.image('bg-woods', 'assets/intro-animation/background-woods.png');
 		this.game.load.image('bg-castle', 'assets/intro-animation/castle-texture.png');
-		this.game.load.image('flyingprincess', 'assets/intro-animation/flyingprincess.png');
+		this.game.load.spritesheet('flyingprincess', 'assets/intro-animation/princessflies.png', c.FLYINGPRINCESS_WIDTH, c.FLYINGPRINCESS_HEIGHT, c.FLYTINGPRINCESS_SPRITES);
 		this.game.load.image('mouse-jetpack', 'assets/intro-animation/mouse-jetpack.png');
 		this.game.load.image('princesssurprise', 'assets/intro-animation/princesssurprise.png');
 		this.game.load.image('bg-sky', 'assets/intro-animation/sky.png');
-		this.game.load.image('towerup', 'assets/intro-animation/tower-up.png');
-		this.game.load.image('towerdown', 'assets/intro-animation/tower-down.png');
+		this.game.load.spritesheet('towerdown', 'assets/intro-animation/towerdown.png', c.TOWER_WIDTH, c.TOWER_HEIGHT, c.TOWER_SPRITES);
+		this.game.load.spritesheet('towerup', 'assets/intro-animation/towerup.png', c.TOWER_WIDTH, c.TOWER_HEIGHT, c.TOWER_SPRITES);
 		this.game.load.spritesheet('hada', 'assets/intro-animation/hada.png', c.HADA_WIDTH,  c.HADA_HEIGHT, c.HADA_SPRITES);
 		this.game.load.spritesheet('mouse', 'assets/intro-animation/mouse.png', c.MOUSE_WIDTH,  c.MOUSE_HEIGHT, c.MOUSE_SPRITES);
 		this.game.load.spritesheet('prince', 'assets/intro-animation/prince.png', c.PRINCE_WIDTH,  c.PRINCE_HEIGHT, c.PRINCE_SPRITES);
@@ -39,26 +39,26 @@ function introState () {
 	function create () {
 
 		this.game.add.sprite(this.game.world.centerX - 200, this.game.world.centerY/2, 'bg-castle');
-		var prince = this.game.add.sprite(this.game.world.centerX - 200, this.game.world.centerY/2, 'prince');
+		var prince = this.game.add.sprite(this.game.world.centerX - 100, this.game.world.centerY/2 + 50, 'prince');
 		prince.animations.add('act');
-		prince.animations.play('act', 2, true);
-		this.game.time.events.add(Phaser.Timer.SECOND * 4.1, function () {addFrame(this, 'bg-sky', 6.3, this.game.world.centerX - 200, this.game.world.centerY/2, 'towerup')}, this);
-		this.game.time.events.add(Phaser.Timer.SECOND * 6.3, function () {addFrame(this, 'bg-grass', 11.7, this.game.world.centerX - 200, this.game.world.centerY/2, 'towerdown')}, this);
-		this.game.time.events.add(Phaser.Timer.SECOND * 11.7, function () {addFrame(this, 'bg-closetower', 15, this.game.world.centerX - 200, this.game.world.centerY/2, 'princesslooking')}, this);
-		this.game.time.events.add(Phaser.Timer.SECOND * 15, function () {addFrame(this, 'bg-woods', 18, this.game.world.centerX - 200, this.game.world.centerY/2, 'hada')}, this);
-		this.game.time.events.add(Phaser.Timer.SECOND * 18, function () {addFrame(this, 'bg-woods', 20.6, this.game.world.centerX - 200, this.game.world.centerY/2, 'mouse')}, this);
-		this.game.time.events.add(Phaser.Timer.SECOND * 18, function () {addFrame(this, 'bg-fartower', 20.6, this.game.world.centerX - 200, this.game.world.centerY/2, 'princessgrabbed')}, this);
-		this.game.time.events.add(Phaser.Timer.SECOND * 20.6, function () {addFrame(this, 'bg-sky', 25, this.game.world.centerX - 200, this.game.world.centerY/2, 'flyingprincess')}, this);
+		prince.animations.play('act', 1, true);
+		this.game.time.events.add(Phaser.Timer.SECOND * 4.1, function () {addFrame(this, 'bg-sky', 6.3, this.game.world.centerX - 200, this.game.world.centerY/2, 'towerdown', 12 , false)}, this);
+		this.game.time.events.add(Phaser.Timer.SECOND * 6.3, function () {addFrame(this, 'bg-grass', 10.7, this.game.world.centerX - 200, this.game.world.centerY/2, 'towerup', 12, false)}, this);
+		this.game.time.events.add(Phaser.Timer.SECOND * 10.7, function () {addFrame(this, 'bg-closetower', 14, this.game.world.centerX - 150, this.game.world.centerY/2, 'princesslooking', 1, true)}, this);
+		this.game.time.events.add(Phaser.Timer.SECOND * 14, function () {addFrame(this, 'bg-woods', 16.5, this.game.world.centerX - 150, this.game.world.centerY/2 + 35, 'hada', 2, true)}, this);
+		this.game.time.events.add(Phaser.Timer.SECOND * 16.5, function () {addFrame(this, 'bg-woods', 18, this.game.world.centerX - 100, this.game.world.centerY/2 + 100, 'mouse', 4, true)}, this);
+		this.game.time.events.add(Phaser.Timer.SECOND * 18, function () {addFrame(this, 'bg-fartower', 20.6, this.game.world.centerX - 200, this.game.world.centerY/2, 'princessgrabbed', 12, true)}, this);
+		this.game.time.events.add(Phaser.Timer.SECOND * 20.6, function () {addFrame(this, 'bg-sky', 25, this.game.world.centerX - 100, this.game.world.centerY/2 + 100, 'flyingprincess', 4, true)}, this);
 		this.game.time.events.add(Phaser.Timer.SECOND * 25, startGame, this);
 
 
 	}
 
-	function addFrame (that, bgimage, timeOnStage, spriteX, spriteY, sprites) {
+	function addFrame (that, bgimage, timeOnStage, spriteX, spriteY, sprites, framerate, loop) {
 		background = that.game.add.sprite(that.game.world.centerX - 200, that.game.world.centerY/2, bgimage);
 		characters = that.game.add.sprite(spriteX, spriteY, sprites);
 		characters.animations.add('animate');
-		characters.animations.play('animate', 4, true);
+		characters.animations.play('animate', framerate, loop, false);
 	}
 
 	function update () {
