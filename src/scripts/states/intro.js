2179485114b3ec5add528abb0aac7d06bb8d5fae
tblanchet <tblanchet@switchfly.com>
2015-09-16 23:24:30 +0000

    small refactor to intro animation

--- src/scripts/states/intro.js
+++ src/scripts/states/intro.js
@@ -32,21 +32,21 @@ function introState () {
         var prince = this.game.add.sprite(this.game.world.centerX - 100, this.game.world.centerY/2 + 50, 'prince');
         prince.animations.add('act');
         prince.animations.play('act', 1, true);
-        events.add(Phaser.Timer.SECOND * 4.1, function () {addFrame(this, 'bg-sky', 6.3, this.game.world.centerX - 200, this.game.world.centerY/2, 'towerdown', 12 , false)}, this);
-        events.add(Phaser.Timer.SECOND * 6.3, function () {addFrame(this, 'bg-grass', 10.7, this.game.world.centerX - 200, this.game.world.centerY/2, 'towerup', 12, false)}, this);
-        events.add(Phaser.Timer.SECOND * 10.7, function () {addFrame(this, 'bg-closetower', 14, this.game.world.centerX - 150, this.game.world.centerY/2, 'princesslooking', 1, true)}, this);
-        events.add(Phaser.Timer.SECOND * 14, function () {addFrame(this, 'bg-woods', 16.5, this.game.world.centerX - 150, this.game.world.centerY/2 + 35, 'hada', 2, true)}, this);
-        events.add(Phaser.Timer.SECOND * 16.5, function () {addFrame(this, 'bg-woods', 18, this.game.world.centerX - 100, this.game.world.centerY/2 + 100, 'mouse', 4, true)}, this);
-        events.add(Phaser.Timer.SECOND * 18, function () {addFrame(this, 'bg-fartower', 20.6, this.game.world.centerX - 200, this.game.world.centerY/2, 'princessgrabbed', 12, true)}, this);
-        events.add(Phaser.Timer.SECOND * 20.6, function () {addFrame(this, 'bg-sky', 25, this.game.world.centerX - 100, this.game.world.centerY/2 + 100, 'flyingprincess', 4, true)}, this);
+        events.add(Phaser.Timer.SECOND * 4.1, function () {addFrame.call(this, { bgimage: 'bg-sky', spriteX: this.game.world.centerX - 200, spriteY: this.game.world.centerY/2, sprites: 'towerdown', framerate: 12 , loop: false})}, this);
+        events.add(Phaser.Timer.SECOND * 6.3, function () {addFrame.call(this, { bgimage: 'bg-grass', spriteX: this.game.world.centerX - 200, spriteY: this.game.world.centerY/2, sprites: 'towerup', framerate: 12, loop: false})}, this);
+        events.add(Phaser.Timer.SECOND * 10.7, function () {addFrame.call(this, { bgimage: 'bg-closetower', spriteX: this.game.world.centerX - 150, spriteY: this.game.world.centerY/2, sprites: 'princesslooking', framerate: 1, loop: true})}, this);
+        events.add(Phaser.Timer.SECOND * 14, function () {addFrame.call(this, { bgimage: 'bg-woods', spriteX: this.game.world.centerX - 150, spriteY: this.game.world.centerY/2 + 35, sprites: 'hada', framerate: 2, loop: true})}, this);
+        events.add(Phaser.Timer.SECOND * 16.5, function () {addFrame.call(this, { bgimage: 'bg-woods', spriteX: this.game.world.centerX - 100, spriteY: this.game.world.centerY/2 + 100, sprites: 'mouse', framerate: 4, loop: true})}, this);
+        events.add(Phaser.Timer.SECOND * 18, function () {addFrame.call(this, { bgimage: 'bg-fartower', spriteX: this.game.world.centerX - 200, spriteY: this.game.world.centerY/2, sprites: 'princessgrabbed', framerate: 12, loop: true})}, this);
+        events.add(Phaser.Timer.SECOND * 20.6, function () {addFrame.call(this, { bgimage: 'bg-sky', spriteX: this.game.world.centerX - 100, spriteY: this.game.world.centerY/2 + 100, sprites: 'flyingprincess', framerate: 4, loop: true})}, this);
         events.add(Phaser.Timer.SECOND * 25, startGame, this);
     }
 
-    function addFrame (that, bgimage, timeOnStage, spriteX, spriteY, sprites, framerate, loop) {
-        background = that.game.add.sprite(that.game.world.centerX - 200, that.game.world.centerY/2, bgimage);
-        characters = that.game.add.sprite(spriteX, spriteY, sprites);
+    function addFrame (frame) {
+        background = this.game.add.sprite(this.game.world.centerX - 200, this.game.world.centerY/2, frame.bgimage);
+        characters = this.game.add.sprite(frame.spriteX, frame.spriteY, frame.sprites);
         characters.animations.add('animate');
-        characters.animations.play('animate', framerate, loop, false);
+        characters.animations.play('animate', frame.framerate, frame.loop, false);
     }
 
 
