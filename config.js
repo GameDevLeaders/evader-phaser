b7ea34f520ca97231c7039c599bab8409710bb70
tonymtz <atony.mtz@gmail.com>
2015-09-24 12:33:41 -0700

    Updated to 0.1.1 * changed canonical url

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
     name: 'Rocket Princess',
-    version: '0.1.0',
+    version: '0.1.1',
     width: '100%',
     height: '100%',
     parent: 'content',
 
     // META
 
-    canonical_url: 'http://rocketprincess.informalpenguins.com/',
+    canonical_url: 'http://play.google.com/store/apps/details?id=com.informalpenguins.rocketprincess',
     keywords: 'rocket, princess, javascript',
     description: 'Vertical scroller, bet your highest score!',
     image: 'http://i.imgur.com/yVs3Uag.png',
